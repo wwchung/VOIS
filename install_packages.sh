5caae313256ead3c6862d64a8cd30db45fa087d9
Guangyu Li <guangyu@umich.edu>
2018-02-21 11:41:56 -0500

    more installation commands

--- install_packages.sh
+++ install_packages.sh
@@ -5,7 +5,16 @@ set -x
 PREV_CREDENTIAL=$HOME"/.credentials/client_secret.json"
 rm -rf $PREV_CREDENTIAL
 
+sudo apt-get update
+sudo apt-get install python3.6
+sudo apt-get install python-pip python-dev build-essential
+pip install --upgrade pip
+pip install --upgrade virtualenv
+
+brew install pkg-config sdl2 sdl2_image sdl2_ttf sdl2_mixer gstreamer
+pip install Cython==0.26.1
 pip install kivy
+pip install https://github.com/kivy/kivy/archive/master.zip
 
 pip install bs4
 
@@ -14,5 +23,3 @@ pip install --upgrade google-api-python-client
 pip install oauth2client
 
 pip install arrow
-
-pip install docx

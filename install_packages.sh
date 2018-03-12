ab4070b7e69525f81a23ce9bce3bb39700ed885d
Daniel Wu <wu.xdaniel@gmail.com>
2018-03-12 19:47:44 -0400

    changed pip to pip3

--- install_packages.sh
+++ install_packages.sh
@@ -7,19 +7,19 @@ rm -rf $PREV_CREDENTIAL
 
 sudo apt-get update
 sudo apt-get install python3.6
-sudo apt-get install python-pip python-dev build-essential
-pip install --upgrade pip
-pip install --upgrade virtualenv
+sudo apt-get install python-pip3 python-dev build-essential
+pip3 install --upgrade pip
+pip3 install --upgrade virtualenv
 
 brew install pkg-config sdl2 sdl2_image sdl2_ttf sdl2_mixer gstreamer
-pip install Cython==0.26.1
-pip install kivy
-pip install https://github.com/kivy/kivy/archive/master.zip
+pip3 install Cython==0.26.1
+pip3 install kivy
+pip3 install https://github.com/kivy/kivy/archive/master.zip
 
-pip install bs4
+pip3 install bs4
 
-pip install --upgrade google-api-python-client
+pip3 install --upgrade google-api-python-client
 
-pip install oauth2client
+pip3 install oauth2client
 
-pip install arrow
+pip3 install arrow

89a7f5b609ec72326ca954775a5bbfbec1504f89
EC2 Default User <ec2-user@ip-172-31-38-99.ec2.internal>
2018-04-11 15:36:29 +0000

    test

--- contacts.py
+++ contacts.py
@@ -4,7 +4,7 @@ import os
 
 class ContactBook:
 
-    def __init__(self, filename = "contacts.p"):
+    def __init__(self, filename = "~/contacts.p"):
         self.filename = filename
         self.contacts = {}
 
@@ -22,71 +22,4 @@ class ContactBook:
         pickle.dump(self.contacts, open(self.filename, "wb" ))
         print("saved to", self.filename)
 
-    def addContact(self, contactname, contact, contacttype = "phone"):
-        contactname = contactname.lower()
-        assert contacttype == "phone" or contacttype == "email"
-
-        if contactname not in self.contacts:
-            self.contacts[contactname] = {"phone": None, "email": None}
-
-        self.contacts[contactname][contacttype] = contact
-        self.saveContact()
-
-    def deleteContact(self, contactname):
-        contactname = contactname.lower()
-        if contactname in self.contacts:
-            self.contacts.pop(contactname, None)
-            self.saveContact()
-
-    def getContact(self, contactname, contacttype = "phone"):
-        contactname = contactname.lower()
-        assert contacttype == "phone" or contacttype == "email"
-
-        try: 
-            retVal = self.contacts[contactname][contacttype]
-            if retVal != None:
-                return retVal
-            else:
-                print("No contact exists for", contactname)
-                return None
-        except KeyError:
-            print("Error: couldn't find person")
-            return None
-            
-    def clearContacts(self):
-        self.contacts = {}
-        self.saveContact()
-
-
-    def listContacts(self):
-        for k,v in self.contacts.items():
-            print(k)
-            print("\tPhone:", v['phone'])
-            print("\tEmail:", v['email'])
-            print()
-
-
-c = ContactBook("contacts.p") 
-
-
-c.addContact('DLJLKJLK', "DLKJLKJLKJDLKJLK")
-c.listContacts()
-c.clearContacts()
-c.addContact("test1", "123")
-c.addContact("test2", "123")
-c.addContact("test3", "123")
-c.addContact("test4", "123")
-
-'''
-c.addContact("Dan", "1234567890")
-c.addContact("Dan", "dwuu@gmail.com", "email")
-c.addContact("Grant", "7818988832")
-c.addContact("Grant", "guangyu@umich.edu", "email")
-c.addContact("voice", "eecs498.vois@gmail.com", "email")
-c.addContact("test", "eecs498.vois@gmail.com", "email")'''
-#c.addContact("Aki", "foo@bar.com", "email")
-c.getContact("Grant", "email")
-# print(c.getContact("Grab"))
-c.listContacts()
-
-
+  
